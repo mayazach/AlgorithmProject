cf4f4880c7beb93b10746e5e50b89d1c2f57280f
mayazaha <mayazaha2@gmail.com>
2017-10-07 17:01:45 +0300

    Checking number of arguments

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
 #include "curve.h"
 
 using namespace std;
 
-int main(){
+int main(int argc, char** argv){
+	ifstream input;
+	ifstream query;
+	ofstream output;
+	bool stat,kflag,lflag;
+	int k = 4, l = 5;
+
+	if(argc > 16 || argc < 10){
+		cout << "Wrong number of arguments" << endl;
+		return 1;
+	}
+
 	Curve a;
 	a.m = 12;
-	cout << a.m << endl;
+	cout << l << endl;
 }
